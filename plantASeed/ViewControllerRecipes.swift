29fb22ba3547e65cbfae3feb5ce03640d362db09
Misha Zatezalo <mishadayla12@gmail.com>
2021-07-22 11:44:49 -0400

    hi

--- plantASeed/ViewControllerRecipes.swift
+++ plantASeed/ViewControllerRecipes.swift
@@ -18,7 +18,7 @@ class ViewControllerRecipes: UIViewController {
 
     /*
     // MARK: - Navigation
-
+//hi
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
