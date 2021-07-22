d979b2ca6138e71f8e0518264edaa17cc3638bc5
Misha Zatezalo <mishadayla12@gmail.com>
2021-07-22 11:33:26 -0400

    pls work

--- plantASeed/SceneDelegate.swift
+++ plantASeed/SceneDelegate.swift
@@ -30,6 +30,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
     }
+    //hi
 
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
