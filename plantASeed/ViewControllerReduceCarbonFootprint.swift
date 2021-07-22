f58effd3cdfe586e7f49ca6342489d010826ae5d
Misha Zatezalo <mishadayla12@gmail.com>
2021-07-22 11:38:17 -0400

    hi

--- plantASeed/ViewControllerReduceCarbonFootprint.swift
+++ plantASeed/ViewControllerReduceCarbonFootprint.swift
@@ -24,6 +24,7 @@ class ViewControllerReduceCarbonFootprint: UIViewController {
     /*
     // MARK: - Navigation
      //j
+     //hi
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
